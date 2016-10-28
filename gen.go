234fcbf1692267713efbf7eefae93dcb237f5835
Rick Morrison <mail@roderickmorrison.net>
2016-10-28 10:36:38 -0700

    gofmt

--- gen.go
+++ gen.go
@@ -1,9 +1,8 @@
 package genstr
 
 import (
-    "io"
-    "crypto/rand"
-
+	"crypto/rand"
+	"io"
 )
 
 var StdChars = []byte("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-_=+,.?/:;{}[]`~")
@@ -11,38 +10,37 @@ var SimpleChars = []byte("ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz2345
 var Digits = []byte("0123456789")
 
 func Standard(length int) string {
-    return rand_char(length, StdChars)
+	return rand_char(length, StdChars)
 }
 
 func Simple(length int) string {
-    return rand_char(length, SimpleChars)
+	return rand_char(length, SimpleChars)
 }
 
 func Number(length int) string {
-    return rand_char(length, Digits)
+	return rand_char(length, Digits)
 }
 
 func rand_char(length int, chars []byte) string {
-    new_pword := make([]byte, length)
-    random_data := make([]byte, length+(length/4)) // storage for random bytes.
-    clen := byte(len(chars))
-    maxrb := byte(256 - (256 % len(chars)))
-    i := 0
-    for {
-        if _, err := io.ReadFull(rand.Reader, random_data); err != nil {
-            panic(err)
-        }
-        for _, c := range random_data {
-            if c >= maxrb {
-                continue
-            }
-            new_pword[i] = chars[c%clen]
-            i++
-            if i == length {
-                return string(new_pword)
-            }
-        }
-    }
-    panic("unreachable")
+	new_pword := make([]byte, length)
+	random_data := make([]byte, length+(length/4)) // storage for random bytes.
+	clen := byte(len(chars))
+	maxrb := byte(256 - (256 % len(chars)))
+	i := 0
+	for {
+		if _, err := io.ReadFull(rand.Reader, random_data); err != nil {
+			panic(err)
+		}
+		for _, c := range random_data {
+			if c >= maxrb {
+				continue
+			}
+			new_pword[i] = chars[c%clen]
+			i++
+			if i == length {
+				return string(new_pword)
+			}
+		}
+	}
+	panic("unreachable")
 }
-
